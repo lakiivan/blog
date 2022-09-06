83e0334f5019de20a424b45e76ab221bc4f6f936
Ivan Lazic <ivanzlazic@gmail.com>
2022-09-06 21:44:58 +0200

    episode 10 finished

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts');
 });
+
+Route::get('posts/{post}', function($slug) {
+    
+    $path = __DIR__."/../resources/posts/{$slug}.html";
+    //ddd($path);
+
+    if (! file_exists($path)) {
+        //dd('File does not exist!');
+        abort(404);
+    }
+
+    $post = cache()->remember("posts.{$slug}", 5, function() use ($path) {
+        var_dump('File get context.');
+        return file_get_contents($path);
+    });
+
+    return view('post', [
+        'post' => $post
+    ]);
+
+})->where('post', '[A-z_\-]+');

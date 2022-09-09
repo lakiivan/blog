c72eb725af3478a5c93616f95bf1ce1973e154b2
Ivan Lazic <ivanzlazic@gmail.com>
2022-09-09 11:28:17 +0200

    episode 27 finished

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace Database\Seeders;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,7 +17,65 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //THIS IS NOT FOR PRODUCTION THIS IS FOR LOCAL ENVIRONMENT AND DEVELOP
+
+        User::truncate();
+        Category::truncate();
+        Post::truncate();
+
+        $user = User::factory()->create();
+
+        $personal = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        $family = Category::create([
+            'name' => 'Family',
+            'slug' => 'family'
+        ]);
+
+        $work = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+
+        Post::create([
+            'user_id'       => $user->id,
+            'category_id'   => $work->id,
+            'title'         => 'My Work Post',
+            'slug'          => 'my-work-post',
+            'excerpt'       => '<p>Excerpt of my working post</p>',
+            'body'          => "<p>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc','slug' => 'my-work-post</p>",
+            'created_at' => '2022-02-14',
+            'updated_at' => '2022-02-14',
+            'published_at' => '2022-02-14'
+        ]); 
+
+        Post::create([
+            'user_id'       => $user->id,
+            'category_id'   => $family->id,
+            'title'         => 'My Family Post',
+            'slug'          => 'my-family-post',
+            'excerpt'       => '<p>Excerpt of my family post</p>',
+            'body'          => "<p>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc','slug' => 'my-work-post</p>",
+            'created_at' => '2022-02-14',
+            'updated_at' => '2022-02-14',
+            'published_at' => '2022-02-14'
+        ]); 
+
+        Post::create([
+            'user_id'       => $user->id,
+            'category_id'   => $personal->id,
+            'title'         => 'My Personal Post',
+            'slug'          => 'my-personal-post',
+            'excerpt'       => '<p>Excerpt of my working post</p>',
+            'body'          => "<p>here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc','slug' => 'my-work-post</p>",
+            'created_at' => '2022-02-14',
+            'updated_at' => '2022-02-14',
+            'published_at' => '2022-02-14'
+        ]); 
+        //\App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
@@ -18,22 +19,36 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
-    $posts = Post::all();
+    //DEBUGGING TOOLS    
+    // \Illuminate\Support\Facades\DB::listen(function ($query) {
+        //first approach
+        // \Illuminate\Support\Facades\Log::info('foo');
+        //second approceh with function logger
+        //logger($query->sql, $query->bindings);
+    // });
+
+    //OLD way with n+1 problem 
+    // $posts = Post::all();
+
+    //new way qithout n+1 problem
+
 
     //ddd($posts[0]->slug);
     return view('posts', [
-        'posts' => $posts 
+        'posts' => Post::with('category')->get()
     ]);
 });
 
-Route::get('posts/{post}', function($slug) {
-    // REFACTORING BIG AND UGLY FIRST VERSION OF WORKING ROUTE
-    //find a post by its slug and pass it to view called "post"
-    $post  = Post::find($slug);
-
+Route::get('posts/{post}', function(Post $post) { // Post::where('slug', $post) -> firstOrFail()
     //dd($post);
     return view('post', [
-        'post' => Post::find($slug)
+        'post' => $post
     ]);
 
+});
+
+Route::get('categories/{category:slug}', function(Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
 });
